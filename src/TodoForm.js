4a7e149c509becbe0b407d1cdb20f7b703d43c5a
marcoweb <marcocarvalho.web@gmail.com>
2021-03-16 23:51:42 +0000

    Componentes

---
+++ src/TodoForm.js
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+export function TodoForm({ addTodo }) {
+    const [value, setValue] = useState("")
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(!value) return;
+        addTodo(value);
+        setValue("")
+    } 
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={value}
+                onChange = {e => setValue(e.target.value)}
+            />
+        </form>
+    )
+}
