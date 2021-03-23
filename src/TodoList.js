4deb0b50f998f61b1649fe68a288d24d661d5c92
marcoweb <marcocarvalho.web@gmail.com>
2021-03-23 23:27:14 +0000

    Firestore

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,37 +1,72 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { TodoItem } from './TodoItem'
 import { TodoForm } from './TodoForm'
 
+import { db } from './index'
+
 export function TodoList() {
     const [todos, setTodos] = useState([
-        {text: "Limpar a casa", isCompleted: false},
-        {text: "Trocar o Óleo do Carro", isCompleted: false},
-        {text: "Fazer a Tarefa da Aula", isCompleted: false}
+        // {text: "Limpar a casa", isCompleted: false},
+        // {text: "Trocar o Óleo do Carro", isCompleted: false},
+        // {text: "Fazer a Tarefa da Aula", isCompleted: false}
     ]);
 
+    function loadItems() {
+        db.collection('todos').get().then((resultados) => {
+            const todoItems = []
+            resultados.forEach((doc) => {
+                todoItems.push({id: doc.id, text: doc.data()['text'], isCompleted: doc.data()['isCompleted']})
+            })
+            setTodos(todoItems)
+        })
+    }
+
     const addTodo = text => {
-        const newTodos = [...todos, {text}];
-        setTodos(newTodos)
+        // const newTodos = [...todos, {text}];
+        // setTodos(newTodos)
+        db.collection('todos').add({
+            text: text,
+            isCompleted: false
+        }).then(() =>{
+            loadItems()
+        })
     }
 
     const removeTodo = index => {
-        const newTodos = [...todos];
-        newTodos.splice(index, 1);
-        setTodos(newTodos);
+        // const newTodos = [...todos];
+        // newTodos.splice(index, 1);
+        // setTodos(newTodos);
+        db.collection('todos').doc(index).delete().then(() =>{
+            loadItems()
+        })
     }
 
     const completeTodo = index => {
-        const newTodos = [...todos];
-        newTodos[index].isCompleted = !newTodos[index].isCompleted;
-        setTodos(newTodos);
+        // const newTodos = [...todos];
+        // newTodos[index].isCompleted = !newTodos[index].isCompleted;
+        // setTodos(newTodos);
+        db.collection('todos').doc(index).get().then((result) => {
+            // console.log(result.data())
+            db.collection('todos').doc(index).update({isCompleted: !result.data()['isCompleted']}).then(() => {
+                loadItems()
+            })
+        })
+        // db.collection('todos').doc(index).update({isCompleted: true}).then(() => {
+        //     loadItems()
+        // })
+        // console.log(index)
     };
+
+    useEffect(() => {
+        loadItems()
+    })
 
     return (
         <div>
             {todos.map((t, index) => (
                 <TodoItem
                      todo={t}
-                     index={index}
+                     index={t['id']}
                      removeTodo={removeTodo}
                      completeTodo={completeTodo}
                 />
--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6h42nF47yEW65q5YZW_E8k4FEbsra558",
+  authDomain: "react-firebase-ex-aaddc.firebaseapp.com",
+  projectId: "react-firebase-ex-aaddc",
+  storageBucket: "react-firebase-ex-aaddc.appspot.com",
+  messagingSenderId: "714873572185",
+  appId: "1:714873572185:web:ffb1635be005c387ba9a48"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+export const db = firebase.firestore()
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
